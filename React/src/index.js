84c790a3b4b762bd64e2b53e792774f3dbb419b5
Raphael <raphaelbarbey@hotmail.co.uk>
2022-06-23 18:28:58 +0200

    added Bootstrap Theme LUX from Bootswatch

--- React/src/index.js
+++ React/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-alert("FOMO NOMORE!!!");
-React.createElement('h1', {}, 'Hello World!');
+ReactDOM.render(
+  React.createElement('h1', {}, 'FOMO!'),
+  document.getElementById('root')
+);
