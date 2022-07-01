7acb1c3b663adf29bcdb102885c007991425b3c9
Raphael <raphaelbarbey@hotmail.co.uk>
2022-07-01 19:06:00 +0200

    created index.css page in public folder to input all our css, also created about page with fomo home button at the bottom of the page

--- re-act/src/pages/About.js
+++ re-act/src/pages/About.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 
 const About = () => 
-    (
-    <div>
-        <h2>About</h2>
-    </div>
-    );
-   
+(
+    <div style= {{ margin: '1em' }} >
+    <h1>About</h1>
+    <div style= {{display: 'flex', flexDirection: 'row' }} >
+        <img className="img" src= " https://images.unsplash.com/photo-1595037935521-15ce2282a03e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZXZlbnR8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" />
+        <div>
+            <h3>Bethel School of Technology Fullstack Bootcamp Final Project</h3>
+            <p><small>Caleb Smith | Matthew De La Cruz | Raphael Liegeois-Barbey </small></p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            
+        </div>
+   </div>
+        <Link to="/">
+            <button type="button" className="btn btn-primary">Fomo</button>
+        </Link>
+ </div>
+);
 
 export default About;
