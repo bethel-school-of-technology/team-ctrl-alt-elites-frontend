a42e2b3a10d07850101cf1459edd870d91c231a7
Raphael <raphaelbarbey@hotmail.co.uk>
2022-07-04 18:04:02 +0200

    created profile page example - will create new page as template from this, that can populate from database entry

--- re-act/src/index.js
+++ re-act/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 import Search from './pages/Search';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
@@ -27,6 +28,9 @@ const App = () => (
           <li style= {{  marginTop: '1em', marginRight: '2em' }}>
             <Link to='/login'>Login</Link>
           </li>
+          <li style= {{  marginTop: '1em', marginRight: '2em' }}>
+            <Link to='/profile'>Profile</Link>
+          </li>
           <li style= {{  marginRight: '2em' }}>
             <Link to='/search'><button type="button" className="btn btn-primary">Search</button></Link>
           </li>
@@ -39,6 +43,7 @@ const App = () => (
         <Route exact path='/'  element={ <Home/> } />
         <Route path="/about" element={<About/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/profile" element={<Profile/>} />
         <Route path="/search" element={<Search/>} />
         </Routes>
       </div>
